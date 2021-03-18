8607e758f36327ee948e2c2306d93306746a0063
Wafa Soda <sodawafa@gmail.com>
2021-03-18 20:45:50 +0100

    test

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -7,6 +7,39 @@ import userEvent from '@testing-library/user-event'
 import firebase from '../__mocks__/firebase'
 import firestore from '../app/Firestore.js'
 
+Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+window.localStorage.setItem('user', JSON.stringify({
+  type: 'Employee',
+  email: 'johndoe@email.com',
+  password: 'azerty',
+  status: 'connected',
+}))
+//const firestore = null
+
+const onNavigate = (pathname) => {
+  document.body.innerHTML = ROUTES({ pathname })
+}
+
+window.alert = (text) => {
+  document.body.innerHTML = text
+}
+
+const newBill = {
+  'id': 1,
+  'vat': '80',
+  'fileUrl': 'https://firebasestorage.googleapis.com/v0/b/billable-677b6.a…f-1.jpg?alt=media&token=c1640e12-a24b-4b11-ae52-529112e9602a',
+  'status': 'accepted',
+  'type': 'Hôtel et logement',
+  'commentAdmin': 'ok',
+  'commentary': 'séminaire billed',
+  'name': 'encore',
+  'fileName': 'preview-facture-free-201801-pdf-1.jpg',
+  'date': '2004-04-04',
+  'amount': 400,
+  'email': 'a@a',
+  'pct': 20,
+}
+
 describe('Given I am connected as an employee', () => {
   describe('When I am on NewBill Page', () => {
     test('Then ...', () => {
@@ -22,20 +55,6 @@ describe('Given I am connected as an employee', () => {
       expect(screen.getByTestId('file')).toBeTruthy()
     })
   })
-
-  Object.defineProperty(window, 'localStorage', { value: localStorageMock })
-  window.localStorage.setItem('user', JSON.stringify({
-    type: 'Employee',
-  }))
-
-  const onNavigate = (pathname) => {
-    document.body.innerHTML = ROUTES({ pathname })
-  }
-  const firestore = null
-
-  window.alert = (text) => {
-    document.body.innerHTML = text
-  }
 
   describe('When bill data is passed to NewBill page', () => {
 
@@ -70,6 +89,7 @@ describe('Given I am connected as an employee', () => {
     })
 
     test('then...', () => {
+
       const html = NewBillUI()
       document.body.innerHTML = html
       const bill = new NewBill({
@@ -78,6 +98,7 @@ describe('Given I am connected as an employee', () => {
         firestore,
         localStorage: window.localStorage,
       })
+      bill.createBill = (bill) => true
       const submit = screen.getByTestId('form-new-bill')
       const handleSubmit = jest.fn(
         (e) => bill.handleSubmit(e))
@@ -88,22 +109,6 @@ describe('Given I am connected as an employee', () => {
         toBe('Fichier n\'est pas acceptée')
     })
 
-    const newBill = {
-      'id': 1,
-      'vat': '80',
-      'fileUrl': 'https://firebasestorage.googleapis.com/v0/b/billable-677b6.a…f-1.jpg?alt=media&token=c1640e12-a24b-4b11-ae52-529112e9602a',
-      'status': 'accepted',
-      'type': 'Hôtel et logement',
-      'commentAdmin': 'ok',
-      'commentary': 'séminaire billed',
-      'name': 'encore',
-      'fileName': 'preview-facture-free-201801-pdf-1.jpg',
-      'date': '2004-04-04',
-      'amount': 400,
-      'email': 'a@a',
-      'pct': 20,
-    }
-
     test('then add bills from mock', async () => {
 
       const html = NewBillUI()
@@ -111,14 +116,45 @@ describe('Given I am connected as an employee', () => {
       const bill = new NewBill({
         document,
         onNavigate,
-        firestore,
+        firestore: null,
         localStorage: window.localStorage,
       })
-      await bill.createBill(newBill)
-      const postSpy = jest.spyOn(firebase, 'post')
-      expect(await firebase.post(newBill)).
+      expect(await bill.createBill(newBill)).toBeUndefined()
+      bill.createBill = async (newBill) => {
+        jest.spyOn(firebase, 'post')
+        return await firebase.post(newBill)
+      }
+      expect(await bill.createBill(newBill)).
         toBe(`The bill has been added with id: ${newBill.id}`)
-      expect(postSpy).toHaveBeenCalled()
+
+    })
+
+    test('then add bills from mock', async () => {
+
+      const html = NewBillUI()
+      document.body.innerHTML = html
+
+      const firestore1 = firestore.bills = () => {
+        this.add = (bill) => {
+          return Promise.resolve({ data: true })
+        }
+      }
+      const bill = new NewBill({
+        document,
+        onNavigate,
+        firestore: firestore1,
+        localStorage: window.localStorage,
+      })
+      //this.firestore.user(user.email).get().then(
+
+      expect(await bill.createBill(newBill)).toBeUndefined()
+      bill.createBill = async (newBill) => {
+        jest.spyOn(firebase, 'post')
+        return await firebase.post(newBill)
+      }
+      expect(await bill.createBill(newBill)).
+        toBe(`The bill has been added with id: ${newBill.id}`)
+
     })
   })
 })
